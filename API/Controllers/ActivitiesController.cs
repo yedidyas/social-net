a7a1cfd6ef3da639a92f8a7432ddb17010a46dc9
yedidya.s <yedidya.s@walkme.com>
2022-01-26 17:38:42 +0200

    move between macs

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -5,27 +5,31 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using System;
+using MediatR;
+using Application.Activities;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
+
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
-        {
-            return await _context.Activities.FindAsync(id);
+        {s
+            return Ok();
         }
     }
 }
